45d7f2ff856f113e5efec96ab71e807f33d5139a
nhatmn2 <38064467+nhatmn2@users.noreply.github.com>
2020-09-10 02:15:42 -0700

    final implementation for all functions

--- Lists/ArrayLists/Main.java
+++ Lists/ArrayLists/Main.java
@@ -21,15 +21,34 @@ public class Main {
 
         //replace with new Employee object --> in this case is the second object
         employeeList.set(1, new Employee("John", "Adams",4568));
-
         //give 1 more empty line
         System.out.println();
-
         //now print the entire ArrayList again 
         employeeList.forEach(employee->System.out.println(employee));
 
         //tell how many employee this will be added 
         System.out.println(employeeList.size());
 
+        //add new object into a specific index
+        employeeList.add(3,new Employee("John", "Doe", 4567));
+        //print the entire list again
+        employeeList.forEach(employee->System.out.println(employee));
+        System.out.println();
+
+        //change from ArrayList to Array -> using toArray() built in method
+        Employee[] employeeArray = employeeList.toArray(new Employee[employeeList.size()]);
+        for(Employee employee: employeeArray){
+            System.out.println(employee);
+        }
+        
+        //this will result in false because they are 2 different objects we created not the same instance. 
+        //For this we need to override the equal and hashcode methods through using auto generate
+        System.out.println(employeeList.contains(new Employee("Mary", "Smith", 22)));
+        System.out.println(employeeList.indexOf(new Employee("John", "Doe", 4567)));
+
+        //remove from arrayList
+        employeeList.remove(2);
+        //print the entire list again
+        employeeList.forEach(employee->System.out.println(employee));
     }
 }
