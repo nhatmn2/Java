d37847c6c93d5ef73341e9340af12ddb0d1429b3
nhatmn2 <38064467+nhatmn2@users.noreply.github.com>
2020-09-29 21:25:17 -0700

    recursive call

---
+++ SortingAlgorithm/Recursion/FactorialAlgorithm.java
@@ -0,0 +1,35 @@
+package SortingAlgorithm.Recursion;
+
+public class FactorialAlgorithm {
+    public static void main (String[] args){
+        System.out.println(iterativeFactorial(3));
+        System.out.println(recursiveFactorial(3));
+    }
+
+
+    //NOTE: be careful when using recursive. If you don't have the exit condition the code will have stack overflow condition.
+    // 1! = 1 * 0! = 1
+    // 2! = 2 * 1 = 2 * 1!;
+    // 3! = 3 * 2 * 1 = 3 * 2!
+    // 4! = 4 * 3 * 2 * 1 = 4 * 3! 
+    // n! = n * (n - 1)!
+    public static int recursiveFactorial(int num){
+        if (num == 0){
+            return 1;
+        }
+        return num * recursiveFactorial(num -1);
+    }
+
+
+    //not going to use recursion
+    public static int iterativeFactorial(int num){
+        if(num == 0){
+            return 1;
+        }
+        int factorial = 1;
+        for (int i = 1; i <= num; ++i){
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
